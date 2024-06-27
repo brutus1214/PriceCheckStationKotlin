68071abf15ce073a28cbd0a4e8dc5aaf13944831
brutus1214 <brutus1214@gmail.com>
2024-06-27 10:07:33 -0400

    First Again

--- app/src/main/java/com/example/pricecheckstationkotlin/view/ui/PriceCheckScreen.kt
+++ app/src/main/java/com/example/pricecheckstationkotlin/view/ui/PriceCheckScreen.kt
@@ -54,6 +54,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.registerReceiver
@@ -673,3 +674,8 @@ fun PriceCheckScreen(decodedData: String?)
     }
 }
 
+@Preview
+@Composable
+fun PriceCheckScreenPreview () {
+    PriceCheckScreen(decodedData = "")
+}
